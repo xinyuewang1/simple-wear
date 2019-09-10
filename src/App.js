168afef0c7ad843f0715fce9d9c3bd754926fc5a
Xinyue Wang <xinyue.wang1@ucdconnect.ie>
2019-09-10 10:36:32 +0100

    finished layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div >
-      
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
