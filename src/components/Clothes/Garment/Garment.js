504a39801e57ff2d56960f9e4360aab00013fe17
Xinyue Wang <xinyue.wang1@ucdconnect.ie>
2019-09-17 16:46:09 +0100

    updating logic for clo

--- src/components/Clothes/Garment/Garment.js
+++ src/components/Clothes/Garment/Garment.js
@@ -1,107 +1,121 @@
 import React from "react";
 
-import garments from "./Garment.module.css";
-
+// props: clo, gender
 const Garment = props => {
   let ensemble = null;
   const clo = props.clo;
   if (clo <= 0.3) {
-    ensemble = ["panties", "t-shirt", "shorts", "light socks", "sandals"];
-  } else if (0.3 < clo <= 0.45) {
+    // 0.23 101
+    ensemble = ["panties", "tubeTop", "shortShorts", "sandals"];
+  } else if (0.3 < clo <= 0.4) {
+    // 0.36 102
     ensemble = [
       "panties",
-      "petticoat",
-      "stockings",
-      "light dress with sleeves",
-      "sandals"
+      "ShortSleeveShirt",
+      "walkingShorts", //0.08
+      "socks",
+      "streetShoes"
     ];
-  } else if (0.45 < clo <= 0.5) {
+  } else if (0.4 < clo <= 0.5) {
+    // 0.49 107
     ensemble = [
-      "underpants",
-      "shirt with short sleeves",
-      "light trousers",
-      "light socks",
-      "shoes"
+      // panties 0.03, brief 0.04
+      "panties",
+      "ShortSleeveShirt", // polo
+      "fittedTrousers",
+      "socks",
+      "streetShoes"
     ];
-  } else if (0.5 < clo <= 0.55) {
+  } else if (0.5 < clo <= 0.6) {
+    // 0.6 108
     ensemble = [
       "panties",
-      "stockings",
-      "shirt with short sleeves",
-      "skirt",
-      "sandals"
+      "shirt", // 0.33
+      "fittedTrousers",
+      "socks",
+      "streetShoes"
     ];
-  } else if (0.55 < clo <= 0.6) {
+  } else if (0.6 < clo <= 0.7) {
+    // 203
     ensemble = [
-      "underpants",
-      "shirt",
-      "light-weight trousers",
+      "panties",
+      "shirt", // 0.33
+      "looseTrousers",
       "socks",
       "shoes"
     ];
-  } else if (0.6 < clo <= 0.7) {
-    if (props.gender === "female") {
-      ensemble = ["panties", "petticoat", "stockings", "dress", "shoes"];
-    } else {
-      ensemble = ["underwear", "shirt", "trousers", "socks", "shoes"];
-    }
-  } else if (0.7 < clo <= 0.75) {
-    ensemble = ["underwear", "sweater", "trousers", "long socks", "runners"];
-  } else if (0.75 < clo <= 0.8) {
-    ensemble = [
-      "panties",
-      "petticoat",
-      "shirt",
-      "skirt",
-      "thick knee socks",
-      "shoes"
-    ];
+  } else if (0.7 < clo <= 0.8) {
+    // 121 0.77
+    ensemble = ["panties", "sweater", "looseTrousers", "socks", "runners"];
   } else if (0.8 < clo <= 0.9) {
+    // 112
     ensemble = [
       "panties",
+      "tShirt",
       "shirt",
-      "skirt",
-      "roundneck sweater",
-      "thick knee socks",
-      "shoes"
+      "looseTrousers",
+      "socks",
+      "streetShoes"
     ];
-  } else if (0.9 < clo <= 0.95) {
+  } else if (0.9 < clo <= 1) {
     ensemble = [
-      "underpants",
-      "singlet with short sleeves",
+      "panties",
+      "tShirt",
+      "shirt",
+      "looseTrousers",
+      "sweater",
+      "socks",
+      "streetShoes"
+    ];
+  } else if (1 < clo <= 1.1) {
+    // 118
+    ensemble = [
+      "panties",
+      "tShirt",
+      "shirt",
+      "thickLooseTrousers",
+      "suiteJacket",
+      "socks",
+      "streetShoes"
+    ];
+  } else if (1.1 < clo <= 1.2) {
+    // 117 1.16
+    ensemble = [
+      "panties",
+      "tShirt",
+      "shirt",
+      "thickLooseTrousers",
+      "vest",
+      "suitJacket",
+      "socks",
+      "streetShoes"
+    ];
+  } else if (1.2 < clo <= 1.3) {
+    // 129 1.3
+    ensemble = [
+      "underwear with long sleeves",
+      "underwear with long legs",
       "shirt",
       "trousers",
       "v-neck sweater",
+      "jacket",
       "socks",
       "shoes"
     ];
-  } else if (0.95 < clo <= 1) {
-    if (props.gender === "female") {
-      ensemble = ["panties", "stockings", "shirt", "skirt", "vest", "jacket"];
-    } else {
-      ensemble = ["panties", "shirt", "trousers", "jacket", "socks", "shoes"];
-    }
-  } else if (1 < clo <= 1.1) {
-    if (props.gender === "female") {
-      ensemble = [
-        "panties",
-        "stockings",
-        "blouse",
-        "long skirt",
-        "jacket",
-        "shoes"
-      ];
-    } else {
-      ensemble = [
-        "underwear",
-        "singlet with short sleeves",
-        "shirt",
-        "trousers",
-        "jacket",
-        "socks",
-        "shoes"
-      ];
-    }
+  } else if (1.3 < clo <= 1.5) {
+    ensemble = [
+      "underwear with short sleeves",
+      "underwear with long legs",
+      "shirt",
+      "trousers",
+      "vest",
+      "jacket",
+      "coat",
+      "socks",
+      "shoes"
+    ];
+  } else {
+    ensemble = null;
   }
 };
 
