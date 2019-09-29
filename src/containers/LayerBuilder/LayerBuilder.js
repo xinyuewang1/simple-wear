a27a7216673b1581a969f85aa11af7db72c58e82
Xinyue Wang <xinyue.wang1@ucdconnect.ie>
2019-09-29 12:40:20 +0100

    start to try to use firebase, not working so far

--- src/containers/LayerBuilder/LayerBuilder.js
+++ src/containers/LayerBuilder/LayerBuilder.js
@@ -11,54 +11,33 @@ const LayerBuilder = () => {
   const [er, setEr] = useState(false);
 
   useEffect(() => {
-    // setTemp(8);
+    setTemp(8);
 
     // Dublin: 207931
     // debugger;
 
-    // console.log(process.env.REACT_APP_ACCU_KEY);
-    // axios
-    //   .get(
-    //     // TODO - Remove this, only for developing, not secure!
-    //     `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}&details=true`
-    //     //     `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}`
-    //   )
-    //   .then(weatherData => {
-    // // TODO - This is hard coded only for current temperature.
-    // if (weatherData.data[0].RealFeelTemperature.Unit === "F") {
-    //   const temperature =
-    //     ((weatherData.data[0].RealFeelTemperature.Value - 32) * 5) / 9;
-    //   setTemp(Math.round(temperature));
-    // } else {
-    //   setTemp(Math.round(weatherData.data[0].RealFeelTemperature.Value));
-    // }
-    //   })
-    //   .catch(error => {
+    // const getTemp = async () => {
+    //   try {
+    //     const weatherData = await axios.get(
+    //       // TODO - Remove this, only for developing, not secure!
+    //       `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}&details=true`
+    //       //     `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}`
+    //     );
+
+    //     // TODO - This is hard coded only for current temperature.
+    //     if (weatherData.data[0].RealFeelTemperature.Unit === "F") {
+    //       const temperature =
+    //         ((weatherData.data[0].RealFeelTemperature.Value - 32) * 5) / 9;
+    //       setTemp(Math.round(temperature));
+    //     } else {
+    //       setTemp(Math.round(weatherData.data[0].RealFeelTemperature.Value));
+    //     }
+    //   } catch (err) {
     //     setEr(true);
-    //   });
+    //   }
+    // };
 
-    const getTemp = async () => {
-      try {
-        const weatherData = await axios.get(
-          // TODO - Remove this, only for developing, not secure!
-          `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}&details=true`
-          //     `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/207931?apikey=${process.env.REACT_APP_ACCU_KEY}`
-        );
-
-        // TODO - This is hard coded only for current temperature.
-        if (weatherData.data[0].RealFeelTemperature.Unit === "F") {
-          const temperature =
-            ((weatherData.data[0].RealFeelTemperature.Value - 32) * 5) / 9;
-          setTemp(Math.round(temperature));
-        } else {
-          setTemp(Math.round(weatherData.data[0].RealFeelTemperature.Value));
-        }
-      } catch (err) {
-        setEr(true);
-      }
-    };
-
-    getTemp();
+    // getTemp();
   }, []);
 
   useEffect(() => {
