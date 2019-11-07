59c705b4a31413d32471dc0426c6e4c8c413910d
Xinyue Wang <xinyue.wang1@ucdconnect.ie>
2019-11-07 16:50:52 +0000

    Throw together AdvStory.js
    But not working, because I haven't figure out how to change url.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 import LayerBuilder from "./containers/LayerBuilder/LayerBuilder";
+import AdvStory from "./components/AdvStory/AdvStory";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Layout>
-          <LayerBuilder />
+          <Route path="/" exact component={LayerBuilder} />
+          <Route path="/adv-story" component={AdvStory} />
+          {/* <LayerBuilder /> */}
         </Layout>
       </div>
     </BrowserRouter>
---
+++ src/components/AdvStory/AdvStory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdvStory = () => {
+  return (
+    <div>
+      <p>Coming soon...(Hopefully)</p>
+    </div>
+  );
+};
+
+export default AdvStory;
