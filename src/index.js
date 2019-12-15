68f836ab9a557511749f433fce1bc7d722334f6a
Xinyue Wang <xinyue.wang1@ucdconnect.ie>
2019-12-15 15:22:27 +0000

    Remove duplicate Layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import "./index.css";
 
@@ -10,7 +11,12 @@ import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
