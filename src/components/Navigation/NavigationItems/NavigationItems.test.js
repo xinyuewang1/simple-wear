6477f11844116f2f24314a2e486e80f11ba35bf5
xinyuewang1 <xinyue.wang1@ucdconnect.ie>
2020-01-03 11:43:16 +0000

    Add test for NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+  it("shoud render 3 <NavigationItem /> elements if not authenticated", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it("shoud render 4 <NavigationItem /> elements if authenticated", () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(4);
+  });
+});
